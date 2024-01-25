5b43498a0868c674fb2f14dd48900338de9dfadb
Cortisol606 <129665546+Cortisol606@users.noreply.github.com>
2024-01-25 12:06:50 +0300

    Delete labs/Lab_10_REACT_Routing/Solutions directory

--- labs/Lab_10_REACT_Routing/Solutions/Task_1/my-app/src/Pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div>Page - About</div>
-  )
-}
-
-export default About
--- labs/Lab_10_REACT_Routing/Solutions/Task_1/my-app/src/Pages/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Main() {
-  return (
-    <div>Page - Main</div>
-  )
-}
-
-export default Main
